c06b36348c1357d2708dbf036cde7ce11e6ba529
Mark <mark@remarkablemark.org>
2019-01-07 22:12:44 -0500

    feat(sprites): add init and update methods to Pipe
    - The init method activates the sprite and moves it the left
    - The update method deactivates the sprite if it goes beyond
      world bounds
    - Reset sprite origin to the top-left

--- src/sprites/Pipe.js
+++ src/sprites/Pipe.js
@@ -1,19 +1,29 @@
 import { Physics } from 'phaser';
 import { TEXTURES } from '../constants';
-import { groups } from '../shared';
 
 export default class Pipe extends Physics.Arcade.Sprite {
   constructor(scene, x, y, texture, frame) {
     super(scene, x, y, TEXTURES.PIPE);
-    groups.pipes.add(this);
 
-    // Add the sprite to the scene.
-    scene.add.existing(this);
+    // Reset drawing position of image to the top-left.
+    this.setOrigin(0);
+  }
 
-    // Enable physics for the sprite.
-    scene.physics.world.enable(this);
+  init() {
+    // Make pipe active and visible.
+    this.enableBody();
 
-    // This stops the sprite from falling away when you jump on it.
-    this.body.immovable = true;
+    // Stop pipe from falling down.
+    this.body.setAllowGravity(false);
+
+    // Move pipe to the left.
+    this.body.setVelocityX(-200);
+  }
+
+  update() {
+    // Make pipe inactive and invisible if it goes beyond world bounds.
+    if (this.x + this.width < 0) {
+      this.disableBody();
+    }
   }
 }
